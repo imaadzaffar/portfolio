fbc5a93711ed2860c17c7a502f1bb420d170186b
imaadzaffar <zafaris.dev@gmail.com>
2021-09-20 11:09:52 +0100

    Added Tailwind and formatting

--- web/client.js
+++ web/client.js
@@ -1,7 +1,7 @@
-import sanityClient from "@sanity/client";
+import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId: "y6l6wadq",
-  dataset: "production",
+  projectId: 'y6l6wadq',
+  dataset: 'production',
   useCdn: true,
-});
+})
