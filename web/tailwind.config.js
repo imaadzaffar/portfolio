9b88e16071f84ffc1441b04e82c90c132946e3e0
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2021-12-25 14:56:25 +0000

    Updated tailwind files

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -14,5 +17,4 @@ module.exports = {
       animation: ['hover'],
     },
   },
-  plugins: [],
 }
