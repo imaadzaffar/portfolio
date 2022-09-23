dfe00c50299d083c66d6fe3e0eea220d4fdbe7f6
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2022-09-22 22:52:55 -0400

    Added social links to top

--- frontend/components/home.js
+++ frontend/components/home.js
@@ -1,11 +1,46 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import { getStrapiMedia } from '../lib/media'
+import {
+  FaGithub,
+  FaLinkedin,
+  FaYoutube,
+  FaInstagram,
+  FaTwitter,
+  FaTiktok,
+} from 'react-icons/fa'
+
+const socials = [
+  {
+    name: 'YouTube',
+    icon: <FaYoutube size={32} />,
+    link: 'https://youtube.com/zafaris',
+  },
+  {
+    name: 'Instagram',
+    icon: <FaInstagram size={32} />,
+    link: 'https://instagram.com/_zafaris',
+  },
+  {
+    name: 'Twitter',
+    icon: <FaTwitter size={32} />,
+    link: 'https://twitter.com/_zafaris',
+  },
+  {
+    name: 'GitHub',
+    icon: <FaGithub size={32} />,
+    link: 'https://github.com/imaadzaffar',
+  },
+  {
+    name: 'LinkedIn',
+    icon: <FaLinkedin size={32} />,
+    link: 'https://linkedin.com/in/imaadzafafr',
+  },
+]
 
 const Home = ({ home }) => {
   const icons = home.data.attributes.icons
   const [iconIndex, setIconIndex] = useState(0)
-  console.log(icons.data[1])
   let icon = icons.data[iconIndex].attributes
 
   function clickIcon() {
@@ -14,17 +49,17 @@ const Home = ({ home }) => {
 
   return (
     <div className="bg-slate-800 flex flex-col items-center justify-center text-center">
-      <nav className="hidden md:block mt-10">
+      {/* <nav className="hidden md:block mt-10">
         {home.data.attributes.navLinks.map((link, index) => (
           <NavLink text={link.text} href={link.href} key={index} />
         ))}
-      </nav>
-      <div className="py-20 md:py-40">
+      </nav> */}
+      <div className="py-20 md:py-32">
         <div className="text-4xl md:text-6xl lowercase font-header text-slate-300">
           {home.data.attributes.greeting}{' '}
           <button
             onClick={() => clickIcon()}
-            className="font-bold lowercase text-sky-400 hover:text-teal-400 transition duration-300"
+            className="font-bold lowercase text-sky-400 hover:text-teal-400 transition duration-300 border-b-2 md:border-b-4 border-sky-400 hover:border-teal-400"
           >
             Imaad
           </button>{' '}
@@ -33,8 +68,20 @@ const Home = ({ home }) => {
             className="inline-block w-[40px] h-[40px] md:w-[60px] md:h-[60px] animate-bounce"
           ></img>
         </div>
-        <div className="mt-4 text-xl md:text-4xl text-slate-400">
+        <div className="mt-4 text-xl md:text-4xl text-slate-500">
           {home.data.attributes.shortDescription}
+        </div>
+        <div className="mt-12 flex flex-row gap-4 justify-center">
+          {socials.map((socialInfo, i) => (
+            <div key={i}>
+              <a
+                className="text-slate-500 hover:text-slate-300 transition-all duration-100 hover:w-[40] hover:h-[40]"
+                href={socialInfo.link}
+              >
+                {socialInfo.icon}
+              </a>
+            </div>
+          ))}
         </div>
       </div>
     </div>
